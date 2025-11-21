4e818114d0ba80affe6d2e0ff50bf803104c882a
minabaghai <mina.baghai@berkeley.edu>
2025-11-20 23:56:07 -0500

    initial skeleton of first website draft

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def w209():
-    file='about9.jpg'
-    return render_template('w209.html',file=file)
+@app.route("/")
+def index():
+    return render_template("index.html")  # Introduction page
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/task_1")
+def task_1():
+    return render_template("task_1.html")
+
+@app.route("/task_2")
+def task_2():
+    return render_template("task_2.html")
+
+@app.route("/task_3")
+def task_3():
+    return render_template("task_3.html")
+
+@app.route("/task_4")
+def task_4():
+    return render_template("task_4.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
